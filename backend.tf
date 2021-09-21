4971c7681a8ecc26c93d4f01921a2c72fbae4a74
Prasad Veginati <prasad.veginati@aspiresys.com>
2021-09-21 08:27:53 +0530

    code change

--- backend.tf
+++ backend.tf
@@ -1,16 +1,5 @@
 
 
-
-
-#
-# Provider Configuration
-#
-
-provider "aws" {
-  region  = "us-east-2"
-  version = ">= 2.38.0"
-}
-
 terraform {
   backend "s3" {
     bucket = "terraform-statebackend"
