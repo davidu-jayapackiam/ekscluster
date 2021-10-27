5d7d39b9131ef7d6961f25ead1466fb6c8ae3556
chavaliInfy <60637623+chavaliInfy@users.noreply.github.com>
2021-10-27 13:21:59 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,10 +2,10 @@
 # Provider Configuration
 #
 
-# provider "aws" {
- # region  = "us-east-2"
-  # version = ">= 2.38.0"
-# }
+ provider "aws" {
+  region  = "us-east-2"
+  version = ">= 2.38.0"
+  }
 
 # Using these data sources allows the configuration to be
 # generic for any region.
@@ -17,10 +17,11 @@ data "aws_availability_zones" "available" {}
 # icanhazip.com to determine local workstation external IP
 # to open EC2 Security Group access to the Kubernetes cluster.
 # See workstation-external-ip.tf for additional information.
-# provider "http" {}
-
+provider "http" {}
+/*
 provider "aws" {
     access_key = "AKIA545UBMJ7LDZLNEO4"
     secret_key = "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"
     region = "us-east-2"
 }
+*/
