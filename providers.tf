773cb6df086190e4da59815fd0b25c630e0ab281
chavaliInfy <60637623+chavaliInfy@users.noreply.github.com>
2021-10-27 10:34:43 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,10 +2,10 @@
 # Provider Configuration
 #
 
-/*provider "aws" {
-  region  = "us-east-2"
+# provider "aws" {
+ # region  = "us-east-2"
   # version = ">= 2.38.0"
- }*/
+# }
 
 # Using these data sources allows the configuration to be
 # generic for any region.
