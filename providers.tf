6d8dbf9ed317d743c6ffc69203dffb94bd9457ea
chavaliInfy <60637623+chavaliInfy@users.noreply.github.com>
2021-10-27 10:07:51 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -17,4 +17,4 @@ data "aws_availability_zones" "available" {}
 # icanhazip.com to determine local workstation external IP
 # to open EC2 Security Group access to the Kubernetes cluster.
 # See workstation-external-ip.tf for additional information.
-provider "http" {}
+# provider "http" {}
